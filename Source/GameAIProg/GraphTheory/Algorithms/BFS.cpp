f2fc85f0b959a610663c9f4672b62e5ca77fa689
Aina Nees Colomina <aina.nees.colomina@student.howest.be>
2026-03-16 08:49:26 +0100

    W05 Pathfinding - BFS FindPath
    - Implemented the FindPath BFS function

--- Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <queue>
+#include <set>
 
 #include "Shared/Graph/Graph.h"
 
@@ -16,5 +17,52 @@ BFS::BFS(Graph* const pGraph)
 std::vector<Node*> BFS::FindPath(Node* const pStartNode, Node* const pDestinationNode) const
 {
 	std::vector<Node*> path;
-	return path;
+	std::queue<Node*> queue[pStartNode];
+	std::set<Node*> visited;
+	std::map<Node*, Node*> parent;
+	
+	visited.insert(pStartNode);
+
+	while (!queue->empty())
+	{
+		Node* node = queue->front();
+		queue->pop();
+		
+		if (node == pDestinationNode)
+		{
+			break;
+		}
+		
+		auto connections = pGraph->FindConnectionsFrom(node->GetId());
+
+		for (Connection* connection : connections)
+		{
+			Node* neighbor = pGraph->GetNode(connection->GetToId()).get();
+
+			if (visited.find(neighbor) == visited.end())
+			{
+				visited.insert(neighbor);
+				parent[neighbor] = node;
+				queue->push(neighbor);
+			}
+		}
+	}
+	
+	
+	if (parent.find(pDestinationNode) == parent.end())
+		return path; // no path found
+
+	Node* current = pDestinationNode;
+
+	while (current != pStartNode)
+	{
+		path.push_back(current);
+		current = parent[current];
+	}
+
+	path.push_back(pStartNode);
+
+	std::reverse(path.begin(), path.end());
+	
+	return path; 
 }
