7beef656b811708ea22f44e0f18869e3fe445087
Aina Nees Colomina <aina.nees.colomina@student.howest.be>
2026-03-22 15:17:04 +0100

    NavMesh Pathfinding - NavGraph
    - NavGraph CreateNavigationGraph function

--- Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
+++ Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
@@ -49,11 +49,67 @@ int GameAI::NavGraph::GetNodeIdFromEdgeIndex(int EdgeIdx) const
 void GameAI::NavGraph::CreateNavigationGraph()
 {
 	//1. Go over all the edges of the navigation mesh and create nodes
-			// Create node here
+	const auto& edges = pNavPoly->GetEdges();
+	const auto& triangles = pNavPoly->GetTriangles();
 
+	for (int edgeIdx = 0; edgeIdx < edges.size(); ++edgeIdx)
+	{
+		const auto& edge = edges[edgeIdx];
+
+		int triangleCount = 0;
+
+		for (const auto& tri : triangles)
+		{
+			if (tri.HasEdge(edge))
+				triangleCount++;
+		}
+
+		if (triangleCount >= 2)
+		{
+			FVector p1 = edge.GetP1(*pNavPoly);
+			FVector p2 = edge.GetP2(*pNavPoly);
+
+			FVector2D middle = FVector2D((p1 + p2) * 0.5f);
+
+			// Create node here
+			AddNode(std::make_unique<NavGraphNode>(middle, edgeIdx));
+		}
+	}
+	
 	//2. Create connections now that every node is created	
 		//2 valid nodes -> 1 connection
 		//3 valid nodes -> 3 connections
-		
+	for (const auto& tri : triangles)
+	{
+		std::vector<int> nodeIds;
+
+		auto triEdges = tri.GetEdges();
+
+		for (const auto& edge : triEdges)
+		{
+			auto edgeIdx = pNavPoly->FindEdgeIndex(edge);
+
+			if (edgeIdx.has_value())
+			{
+				int nodeId = GetNodeIdFromEdgeIndex(edgeIdx.value());
+
+				if (nodeId != Graphs::InvalidNodeId)
+					nodeIds.push_back(nodeId);
+			}
+		}
+
+		if (nodeIds.size() == 2)
+		{
+			AddConnection(nodeIds[0], nodeIds[1]);
+		}
+		else if (nodeIds.size() == 3)
+		{
+			AddConnection(nodeIds[0], nodeIds[1]);
+			AddConnection(nodeIds[1], nodeIds[2]);
+			AddConnection(nodeIds[2], nodeIds[0]);
+		}
+	}
+	
 	//3. Set the connections cost to the actual distance
+	SetConnectionCostsToDistances();
 }
